1f0f6fd25f2ea9118ac59303b029a2ba37f0031f
EhyunHwaNyeong <dltjrrb4321@gmail.com>
2025-08-04 15:43:48 +0900

    4 123

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
-{
+{    
     public float rotationSpeed = 0;
+    public GameObject onCollectEffect;
     void Start()
     {
-        
+
     }
     void Update()
     {
         transform.Rotate(0, rotationSpeed, 0);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Destroy the collectible
+            Destroy(gameObject);
+
+            // istantiate the particle effect
+            Instantiate(onCollectEffect,
+                transform.position,
+                transform.rotation);
+        }
     }
 }
